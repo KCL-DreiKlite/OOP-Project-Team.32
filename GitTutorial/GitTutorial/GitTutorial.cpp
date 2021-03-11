da2a27c5128cd5521ef2a6a7468bac4f31059d2d
lcy01280 <t108590040@ntut.org.tw>
2021-03-12 01:33:54 +0800

    choose a number

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -7,6 +7,13 @@
 //using namespace System;
 
 bool Guess(int number) {
+	static int target =-1;
+	srand(time(NULL));
+	if(target==-1){
+		target =rand() %100+1;
+	}
+	if(number==target)
+		return true;
 	return false;
 }
 int main() {
